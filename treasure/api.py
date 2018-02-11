e2882659b95b901e6f7542296740de97e4617e81
Chris Proctor <chris.proctor@gmail.com>
2018-02-11 16:03:36 -0500

    Adding client api and client CLI

---
+++ treasure/api.py
@@ -0,0 +1,137 @@
+import requests
+
+URLS = {
+    'new_player': 'players/new/{name}',
+    'show_player': 'players/{pid}',
+    'new_game': 'players/{pid}/games/new',
+    'join_game': 'players/{pid}/games/{gid}/join',
+    'resume_game': 'players/{pid}/games/resume',
+    'join_any_game': 'players/{pid}/games/join',
+    'show_game': 'players/{pid}/games/{gid}',
+    'play_move': 'players/{pid}/games/{gid}/play/{play}'
+}
+
+class TreasureApi():
+    def __init__(self, url, pid=None, name=None):
+        "Initialized with either an existing pid or a name to add"
+        if not url.endswith('/'):
+            url = url + '/'
+        self.url = url
+
+        if (pid and name) or not (pid or name):
+            raise ValueError("Must provide either a pid (for existing user) or a name (for new user)")
+        if pid:
+            self.get_player(pid)
+        else:
+            self.new_player(name)
+
+    def __str__(self):
+        if not hasattr(self, 'player'):
+            return "API"
+        if not hasattr(self, 'game'):
+            return "API player:{} ({})".format(self.player['name'], self.player['pid'])
+        return  "API player:{} ({}) game: {}".format(self.player['name'], self.player['pid'], self.game['gid'])
+
+    def __repr__(self):
+        return "<{}>".format(self)
+
+    def get_url(self, url, **kwargs):
+        return self.url + URLS[url].format(**kwargs)
+
+    def save_player(self, response):
+        if response.ok:
+            self.player = response.json()
+            return self.player
+        else:
+            self.error = response.json()
+
+    def save_game(self, response):
+        if response.ok:
+            self.game = response.json()
+            return self.game
+        else:
+            self.error = response.json()
+
+    def new_player(self, name):
+        return self.save_player(requests.get(self.get_url('new_player', 
+            name=name
+        )))
+
+    def get_player(self, pid=None):
+        return self.save_player(requests.get(self.get_url('show_player', 
+            pid=pid or self.player['pid']
+        )))
+
+    def new_game(self, pid=None):
+        return self.save_game(requests.get(self.get_url('new_game', 
+            pid=pid or self.player['pid']
+        )))
+
+    def join_game(self, pid=None, gid=None):
+        return self.save_game(requests.get(self.get_url('join_game', 
+            pid=pid or self.player['pid'], 
+            gid=gid or self.game['gid']
+        )))
+
+    def resume_game(self, pid=None):
+        return self.save_game(requests.get(self.get_url('resume_game', 
+            pid=pid or self.player['pid']
+        )))
+
+    def join_any_game(self, pid=None):
+        return self.save_game(requests.get(self.get_url('join_any_game', 
+            pid=pid or self.player['pid']
+        )))
+
+    def get_game(self, pid=None, gid=None):
+        return self.save_game(requests.get(self.get_url('show_game', 
+            pid=pid or self.player['pid'], 
+            gid=gid or self.game['gid']
+        )))
+
+    def can_play(self):
+        if not self.player and self.game:
+            raise ValueError("There is no current game")
+        return self.game['status'] == 'playing' and self.player['name'] not in self.game['turns'][0].keys()
+
+
+    def play_move(self, play, pid=None, gid=None):
+        return self.save_game(requests.get(self.get_url('play_move', 
+            play=play,
+            pid=pid or self.player['pid'], 
+            gid=gid or self.game['gid']
+        )))
+
+    def opponent_name(self):
+        if not self.player and self.game:
+            raise ValueError("There is no current game")
+        for p in self.game['players'].keys():
+            if p not in [self.player['name'], 'treasure']:
+                return p
+
+    def last_complete_turn(self):
+        if not self.player and self.game:
+            raise ValueError("There is no current game")
+        for turn in self.game['turns']:
+            if len(turn.items()) == 3:
+                return turn
+
+    def last_turn_result(self):
+        turn = self.last_complete_turn()
+        myPlay = turn[self.player['name']]
+        theirPlay = turn[self.opponent_name()]
+        if myPlay == theirPlay:
+            return 0
+        if myPlay == 13 and theirPlay == 1:
+            return -1
+        if myPlay == 1 and theirPlay == 13:
+            return 1
+        if myPlay > theirPlay:
+            return 1
+        else:
+            return -1
+
+if __name__ == '__main__':
+    api = TreasureApi('http://localhost:8000', pid=633311)
+    api2 = TreasureApi('http://localhost:8000', pid=211166)
+
---
+++ treasure/client.py
@@ -0,0 +1,100 @@
+from api import TreasureApi
+from argparse import ArgumentParser
+from time import sleep
+
+
+class TreasureClient:
+    def __init__(self, url=None, pid=None, name=None, gid=None):
+        self.api = TreasureApi(url=args.url, pid=args.pid, name=args.name)
+        if gid and gid in api.player['games_playing']:
+            self.api.get_game(gid=gid)
+        else:
+            self.select_game()
+
+    def select_game(self):
+        print("How would you like to join a game?")
+        while not self.prompt_choices([
+            ('n', 'new game', self.new_game),
+            ('r', 'resume an ongoing game', self.api.resume_game),
+            ('j', 'join any game', self.api.join_any_game),
+            ('s', 'join a specific game', self.join_game)
+        ]):
+            print("Sorry, that didn't work. Please choose an option:")
+
+        print("Playing game {} against {}".format(self.api.game['gid'], self.api.opponent_name()))
+        self.play_turn()
+
+    def new_game(self):
+        self.api.new_game()
+        return self.wait_for_game_to_start()
+
+    def wait_for_game_to_start(self):
+        self.api.get_game()
+        if self.api.game['status'] == 'playing':
+            return True
+        print("Waiting for another player to join.")
+        return self.prompt_choices([
+            ('w', 'keep waiting', self.wait_for_game_to_start),
+            ('o', 'choose another game', self.select_game)
+        ])
+
+    def join_game(self):
+        return self.api.join_game(input("Game ID: "))
+
+    def play_turn(self):
+        if self.api.can_play():
+            hand = self.api.game['players'][self.api.player['name']]['hand']
+            print("Treasure is {}. Choose a play. (Your hand is {}.)".format(
+                self.api.game['turns'][0]['treasure'], 
+                ", ".join(str(card) for card in hand)
+            ))
+            while not self.api.play_move(play=input("> ")):
+                print("That's not a valid choice.")
+
+        if self.api.game['status'] == 'playing':
+            print("Waiting for opponent's move...")
+        while self.api.game['status'] == 'playing' and not self.api.can_play():
+            self.api.get_game()
+            sleep(1)
+
+        print("{} plays {}. You {}.".format(
+            self.api.opponent_name(),
+            self.api.last_complete_turn()[self.api.opponent_name()],
+            {-1: 'lose', 0: 'tie', 1: 'win'}[self.api.last_turn_result()]
+        ))
+        print("Your score: {}. {}'s score: {}".format(
+            self.api.game['players'][self.api.player['name']]['score'],
+            self.api.opponent_name(),
+            self.api.game['players'][self.api.opponent_name()]['score'],
+        ))
+        if self.api.game['status'] == 'playing':
+            self.play_turn()
+        else:
+            self.end_game()
+
+    def end_game(self):
+        print("Game over. Your score: {}; Opponent's score: {}".format(
+            self.api.game['players'][self.api.player['name']]['score'],
+            self.api.game['players'][self.api.opponent_name()]['score'],
+        ))
+        self.select_game()
+        
+    def prompt_choices(self, choices):
+        for key, message, action in choices:
+            print("  [{}] {}".format(key, message))
+        while True:
+            choice = input("> ")
+            for key, message, action in choices:
+                if choice == key: 
+                    return action()
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='Play treasure')
+    parser.add_argument('--url', default="http://localhost:8000", help="treasure server url")
+    parser.add_argument('--pid', help="player id, if already existing")
+    parser.add_argument('--name', help="player name, if creating a new player")
+    parser.add_argument('--gid', help="game id, if already existing")
+    args = parser.parse_args()
+    client = TreasureClient(url=args.url, pid=args.pid, name=args.name, gid=args.gid)
+
